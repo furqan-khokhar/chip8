18d98f2412031041efd65240bd2f9e7f5695bd31
Furqan khokhar <furqan.a.khokhar@gmail.com>
2025-10-07 10:16:52 +0100

    feat: added specifications
    Created variables representing the specifications of the chip8 emulator.

--- chip8/chip8/cpu.h
+++ chip8/chip8/cpu.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef CPU_H
+#define CPU_H
+
+#include <stdint.h>					// Standard library for fixed width integer types
+
+class cpu {
+private:
+	uint8_t memory[4 * 1024];		// Memory (4 kilobytes)
+									// All of chip8's memory is RAM and considered writable
+
+	uint8_t gfx[64 * 32];			// Graphics Buffer
+									// 64 x 32 pixel resolution
+									// Each byte represents 1 pixel, allows for colours in future implementation
+
+	uint16_t pc;					// Program Counter
+
+	uint16_t i;						// Index Register
+									// Can only address 12 bits
+
+	uint16_t stack[16];				// Stack
+									// Calls subroutines and returns from them
+
+	uint8_t delay_timer;			// Delay Timer
+									// Decremented at 60Hz until it reaches 0
+
+	uint8_t sound_timer;			// Sound Timer
+									// Decremented at 60Hz until it reaches 0
+									// Beeps if not at 0
+
+	uint8_t v[16];					// Variable Registers
+									// Numbered 0 through F
+									// VF is used as a flag register
+
+	uint16_t opcode;				// Opcode
+									// Instructions/data to be processed
+};
+#endif
