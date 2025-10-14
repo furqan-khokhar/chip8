0be205dfa01c100ddab89d8ee9b2602f73fff092
Furqan khokhar <furqan.a.khokhar@gmail.com>
2025-10-14 10:26:27 +0100

    feat: reading rom files
    Added file reading but it currently cannot be outputted back to the console.

--- chip8/chip8/main.cpp
+++ chip8/chip8/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <stdint.h>
+#include <vector>
+#include <fstream>
+#include <filesystem>
 
 #include "cpu.h"
 
-int main() {
+auto read_file_data(const char* file_path) noexcept -> std::vector<char> {
+	std::vector<char> file_data{};
+
+	std::ifstream input_file(file_path, std::ios::binary);
+
+	auto size = std::filesystem::file_size(file_path);
+
+	std::cout << size << std::endl;
+
+	input_file.seekg(0, std::ios::end);
+
+	//std::cout << std::hex << input_file.rdbuf() << std::endl;
+
+	std::istreambuf_iterator<char> input_reader{ input_file }, end;
+
+	file_data.assign(input_reader, {});
+
+	std::string input_text{ input_reader, end };
+
+	input_file.close();
+
+	//std::cout << input_text.size() << std::endl;
+
+	return file_data;
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 2) {
+		std::cout << "[ * ] Load Chip8 ROM\n";
+		return 1;
+	}
+
+	//char* path = argv[1];
+
+	//std::cout << path << std::endl;
+
+	read_file_data(argv[1]);
 	
 	Cpu cpu = Cpu();	// Initialise chip8 cpu
 
